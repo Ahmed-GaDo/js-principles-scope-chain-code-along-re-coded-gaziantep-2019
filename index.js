85c3c47dcaf588e4197dbf118834a6d6c50e9766
Ahmed <a.gado.2df@gmail.com>
2019-07-25 04:29:12 +0300

    scope Chain Exercise Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 /* Your code goes here */
+var customerName = 'bob';
+function upperCaseCustomerName (){
+    customerName = 'BOB'
+}
+var bestCustomer ;
+function setBestCustomer (){
+    bestCustomer = 'not bob' ;
+}
+function overWriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'Ahmed' ;
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mido';
+    console.log(leastFavoriteCustomer);
+}
+let favoriteCustomer = 'ahmed';
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'mido';
+
+  throw SyntaxError(SyntaxError)
+}
