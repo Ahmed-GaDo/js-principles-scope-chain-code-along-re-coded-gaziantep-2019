959bc7ef2c83cc041b1c3a494ff08e06f50464d8
Ahmed Gado <a.gado.2df@gmail.com>
2019-07-25 01:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function setBestCustomer (){
     bestCustomer = 'not bob' ;
 }
 function overwriteBestCustomer(){
-    bestCustomer = 'Ahmed'
+    bestCustomer = 'maybe bob'
 }
 const leastFavoriteCustomer = 'Ahmed' ;
 function changeLeastFavoriteCustomer(){
